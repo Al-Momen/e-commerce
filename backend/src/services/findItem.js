079c61f02d9ad8121ed967d422e2a0fbdc2f85fd
AL-Momen <58368636+Al-Momen@users.noreply.github.com>
2025-11-25 20:01:09 +0600

    error set

--- backend/src/services/findItem.js
+++ backend/src/services/findItem.js
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
-
+import { createError } from "../../helper/helper.js";
 const findItem = async (Model, id, options = {}) => {
     try {
         const item = await Model.findById(id, options);
         if (!item) {
-            const error = new Error(`${Model.modelName} Item not found`);
-            error.status = 404;
-            throw error;
+            createError(`${Model.modelName} Item not found`, 404);
         }
         return item;
     } catch (error) {
         if (error instanceof mongoose.Error) {
-            error = new Error("Database Internal Error");
-            error.status = 500;
+            createError(`Database Internal Error`, 500);
         }
         throw error;
     }
